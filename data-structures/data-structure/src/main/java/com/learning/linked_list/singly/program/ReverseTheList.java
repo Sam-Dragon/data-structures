59cb72968bc97204884e7550271ec9d6522c78b6
Sam-Dragon <rahulsheth2013@gmail.com>
2026-01-25 18:41:58 +0530

    # Master
    - Reverese the List

---
+++ data-structures/data-structure/src/main/java/com/learning/linked_list/singly/program/ReverseTheList.java
@@ -0,0 +1,83 @@
+package com.learning.linked_list.singly.program;
+
+public class ReverseTheList {
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null, tail = null;
+
+    private void prepare() {
+        Node node = new Node(40);
+        node.next = head;
+        head = node;
+
+        node = new Node(30);
+        node.next = head;
+        head = node;
+
+        node = new Node(20);
+        node.next = head;
+        head = node;
+
+        node = new Node(10);
+        node.next = head;
+        head = node;
+    }
+
+    private void display() {
+        if (head == null) {
+            return;
+        }
+
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " --> ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    // REVERSE THE LIST
+    private void reverse() {
+        if (head == null) {
+            throw new RuntimeException("Empty List");
+        }
+
+        if (head.next == null) {
+            System.out.println(head.data);
+            return;
+        }
+
+        Node prev = null, curr = head;
+        while (curr != null) {
+            // Holding Current Pointer Reference
+            Node currNext = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = currNext;
+        }
+
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+        ReverseTheList list = new ReverseTheList();
+        list.prepare();
+        list.display();
+
+        System.out.println("\nReverse the list");
+        list.reverse();
+        list.display();
+    }
+
+
+}
