3fc675318f568126658852d8118c972ebea68f73
Sam-Dragon <rahulsheth2013@gmail.com>
2026-04-16 22:18:14 +0530

    # Master
    - stack problem with solutions updated

---
+++ data-structures/data-structure/src/main/java/com/learning/stack/programs/PrefixExpression.java
@@ -0,0 +1,68 @@
+package com.learning.stack.programs;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PrefixExpression {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String input = sc.nextLine();
+        System.out.println("Input = " + input);
+
+        String expression = convertToPrefixExpression(input);
+        System.out.println("Prefix Expression for Input '" + input + "' = " + expression);
+    }
+
+    private static String convertToPrefixExpression(String input) {
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> symbols = new Stack<>();
+        StringBuilder builder = new StringBuilder();
+
+        // Push the elements based on alphabets and symbols
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+            if (Character.isAlphabetic(character))
+                stack.push(character);
+            else
+                symbols.push(character);
+        }
+
+        if (stack.size() - 1 != symbols.size()) {
+            throw new RuntimeException("Please give Proper Exception");
+        }
+
+//        System.out.println(stack);
+//        System.out.println(symbols);
+
+        // Process
+        String second = String.valueOf(stack.pop());
+        while (!symbols.isEmpty() && !stack.isEmpty()) {
+            String first = String.valueOf(stack.pop());
+            Character symbol = symbols.pop();
+
+            second = process(second, first, symbol);
+        }
+        builder.append(second);
+
+        return builder.toString();
+    }
+
+    private static String process(String second, String first, Character symbol) {
+        switch (symbol) {
+            case '+':
+                return "+" + first + second;
+            case '-':
+                return "-" + first + second;
+            case '*':
+                return "*" + first + second;
+            case '/':
+                return "/" + first + second;
+            case '^':
+                return "^" + first + second;
+            default:
+                return "";
+        }
+    }
+}
